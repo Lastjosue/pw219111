f0bec07811348bff2ba869424acd445e8ebe35f4
Lastjosue <roly.1998@hotmail.es>
2019-04-30 11:59:41 -0600

    Ruta inicial y controlador list

--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const rutas = express.Router();
+const customerController = require('../controllers/customerController');
 
 //get es lo que solicita el navegador del cliente
-rutas.get("/",(req,res)=>{
-	res.send("Hola mundo Node");
-})
+// rutas.get("/",(req,res)=>{
+// 	res.send("Hola mundo Node");
+// })
+// module.exports = rutas;
+
+rutas.get('/', customerController.list);
+// rutas.post('/add', customerController.save);
+// rutas.get('/delete/:id', customerController.delete);
+// rutas.get('/update/:id', customerController.edit); 
+// rutas.get('/update/:id', customerController.update); 
+
 module.exports = rutas;
