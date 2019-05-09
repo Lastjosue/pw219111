dc6467ef46e38a3e89583674d041e85154f8c563
Lastjosue <roly.1998@hotmail.es>
2019-05-09 11:54:41 -0600

    Frutas 2

--- vue_ejemplos/js/primervue.js
+++ vue_ejemplos/js/primervue.js
@@ -1,13 +1,40 @@
+//la constante app, es la id del div del html con el que interactuaremos
 const app = new Vue({
-	el: "#app",
+	el: '#app',
 	data: {
-		titulo: 'Hola mundo con VUE',
-		frutas: ['manzanas','platano','peras','mango'],
+		//Variable normal
+		titulo: 'Hola Mundo con Vue',
+		//Arreglo de variables
+		frutas: ['Manzana','Uva','Peras','Fresas'],
+		//Arreglo de objetos
 		frutas2: [
-			{nombre:'manzana', cantidad: 10},
-			{nombre:'platano', cantidad: 0},
-			{nombre:'peras', cantidad: 20},
-			{nombre:'mango', cantidad: 80}
-		]
+			{nombre: 'Manzana', cantidad: 11},
+			{nombre: 'Uva', cantidad: 0},
+			{nombre: 'Peras', cantidad: 80},
+			{nombre: 'Fresas', cantidad: 110}
+		],
+		nuevaFruta: '',
+		total: 0
+	},
+	methods: {
+		agregarFruta(){
+			// console.log("diste clic");
+			this.frutas.push(this.nuevaFruta);
+			this.frutas2.push({
+				nombre: this.nuevaFruta, 
+				cantidad: 0
+			});
+			this.nuevaFruta = '';
+		}
+
+	},
+	computed: {
+		sumarFrutas(){
+			this.total = 0;
+			for(fruta of this.frutas2){
+				this.total = this.total+fruta.cantidad;
+			}
+			return this.total;
+		}
 	}
 });
